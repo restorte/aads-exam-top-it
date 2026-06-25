0f92545e82709fbc4e1f9d16d7e81bf82bfa19b0
restorte <vanyakarpenko837@yandex.ru>
2026-06-25 11:22:12 +0300

    add implementation main

--- karpenko.ivan/U1/main.cpp
+++ karpenko.ivan/U1/main.cpp
@@ -1,4 +1,33 @@
-int main()
-{
-    
+#include "person.hpp"
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+int main() {
+    karpenko::Node* head = NULL;
+    karpenko::Node* tail = NULL;
+    size_t accepted = 0;
+    size_t ignored = 0;
+
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        karpenko::Person p;
+        bool ok = karpenko::parseLine(line, p);
+        if (!ok) {
+            ++ignored;
+            continue;
+        }
+        bool added = karpenko::addPerson(head, tail, p);
+        if (added) {
+            ++accepted;
+        } else {
+            ++ignored;
+        }
+    }
+
+    karpenko::printList(head, std::cout);
+    karpenko::printStats(accepted, ignored, std::cerr);
+
+    karpenko::clearList(head);
+    return 0;
 }
