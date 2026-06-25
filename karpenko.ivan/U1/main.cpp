3975f068ac7c03b6dd0b903d1daa54719600dc43
restorte <vanyakarpenko837@yandex.ru>
2026-06-25 11:24:21 +0300

    add parseArgs

--- karpenko.ivan/U1/main.cpp
+++ karpenko.ivan/U1/main.cpp
@@ -3,7 +3,36 @@
 #include <string>
 #include <cstdlib>
 
-int main() {
+static bool parseArgs(int argc, char* argv[], std::string& inFile, std::string& outFile, bool& hasIn, bool& hasOut) {
+    if (argc > 3) {
+        return false;
+    }
+
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if (arg.compare(0, 3, "in:") == 0) {
+            if (hasIn) return false;
+            hasIn = true;
+            inFile = arg.substr(3);
+        } else if (arg.compare(0, 4, "out:") == 0) {
+            if (hasOut) return false;
+            hasOut = true;
+            outFile = arg.substr(4);
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(int argc, char* argv[]) {
+    std::string inFile, outFile;
+    bool hasIn = false, hasOut = false;
+
+    if (!parseArgs(argc, argv, inFile, outFile, hasIn, hasOut)) {
+        return 1;
+    }
+
     karpenko::Node* head = NULL;
     karpenko::Node* tail = NULL;
     size_t accepted = 0;
